8af34ca519c9c11f6dfee1e7ba8c5562798b35ce
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-12-22 19:00:15 +0800

    feat: remove webpack from config

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,21 @@
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
-// @ts-ignore
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  devIndicators: false,
-  experimental: {
-    authInterrupts: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
+	/* config options here */
+	images: {
+		domains: ["lh3.googleusercontent.com"],
+	},
+	devIndicators: false,
+	experimental: {
+		authInterrupts: true,
+	},
+	// webpack: (config, { isServer }) => {
+	// 	if (isServer) {
+	// 		config.plugins = [...config.plugins, new PrismaPlugin()];
+	// 	}
+	// 	return config;
+	// },
 };
 
 export default nextConfig;
